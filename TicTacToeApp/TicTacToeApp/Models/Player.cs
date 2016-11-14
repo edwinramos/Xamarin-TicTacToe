dc3f04988211468e9246f3927091deaf5a9f8dcc
Edwin Ramos <edwinramos.93@hotmail.com>
2016-11-13 23:36:50 -0400

    first commit

---
+++ TicTacToeApp/TicTacToeApp/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeApp.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public int Score { get; set; }
+    }
+}
